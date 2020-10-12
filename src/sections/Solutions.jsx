da8d2698de424f5f310eba823e633fdf912c4066
Riken Shah <rikenshah.02@gmail.com>
2020-10-12 11:51:30 +0530

    ✨  Added Solution Component

---
+++ src/sections/Solutions.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+function Solutions() {
+  return (
+    <div className="solution-wrapper" id="solutions">
+      <div className="item">
+        <h1 data-text="Solutions" className="title">
+          Solutions
+        </h1>
+        <span className="description">
+          I’m here to help you get more clients so you can focus on the reason you started your business in the first place: to help people from all over the world.
+          <br />
+          <span className="extra">
+            Shows your new client  a clear path to working with you Inspires them to take action and buy from you Find out more
+          </span>
+        </span>
+      </div>
+    </div>
+  );
+}
+
+
+
+
+export default Solutions
